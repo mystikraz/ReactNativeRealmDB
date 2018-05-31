766ba4ae21bb4833b57259de88c937e1d63e6230
Raj Tandukar <royal_raj@outlook.com>
2018-05-31 20:13:22 +0545

    add extra columns for migration test

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import React, { Component } from 'react';
 import {
   StyleSheet, Platform, View, Image, Text, TextInput, TouchableOpacity, Alert
 } from 'react-native';
-
 var Realm=require('realm');
 let realm;
 
@@ -20,7 +19,8 @@ export default class App extends Component<Props> {
     this.state={
       Student_Name:'',
       Student_Class:'',
-      Student_Subject:''
+      Student_Subject:'',
+      Student_Interest:'',
     }
 
     realm=new Realm({
@@ -29,10 +29,26 @@ export default class App extends Component<Props> {
         student_id:{type:'int',default:0},
         student_name:'string',
         student_class:'string',
-        student_subject:'string'
-      }}]
+        student_subject:'string',
+        student_interest:'string',
+        student_test:'string',
+      }}], schemaVersion:3
     });
+    // const Student_Info={
+    //   name:'Student_Info',
+    //   properties:{
+    //        student_id:{type:'int',default:0},
+    //     student_name:'string',
+    //     student_class:'string',
+    //     student_subject:'string',
+    //   }
+    // };
+
+
+
+    // let realmSchema=new Realm({schema:[Student_Info], schemaVersion:1});
   }
+  
 
   add_Student=()=>{
     realm.write(()=>{
@@ -42,7 +58,8 @@ export default class App extends Component<Props> {
         student_id:ID,
         student_name:this.state.Student_Name,
         student_class:this.state.Student_Class,
-        student_subject:this.state.Student_Subject
+        student_subject:this.state.Student_Subject,
+        student_interest:this.state.Student_Interest,
       });
     });
     Alert.alert("Student details added sucessfully.");
@@ -74,6 +91,12 @@ export default class App extends Component<Props> {
                 style = { styles.TextInputStyle } 
                 underlineColorAndroid = "transparent" 
                 onChangeText = { ( text ) => { this.setState({ Student_Subject: text })} } 
+              />
+          <TextInput 
+                placeholder="Enter Student Interest"
+                style = { styles.TextInputStyle } 
+                underlineColorAndroid = "transparent" 
+                onChangeText = { ( text ) => { this.setState({ Student_Interest: text })} } 
               />
  
  
